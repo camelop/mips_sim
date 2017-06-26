59c53a7ab09e154137ebce97beafbffac6e3cb89
Little Round <lxy9843@qq.com>
2017-06-26 19:54:08 +0800

    继续完善parser，测试函数指针

---
+++ code/mips_sim/mips_sim/Token.hpp
@@ -0,0 +1,10 @@
+#ifndef littleround_MIPS_token
+#define littleround_MIPS_token
+
+class Token {
+	//to construct the map's latter element
+	//give the information pakage of the token
+	//also get the key to the function
+};
+
+#endif
--- code/mips_sim/mips_sim/program.hpp
+++ code/mips_sim/mips_sim/program.hpp
@@ -2,12 +2,50 @@
 #define littleround_MIPS_program
 
 #include <vector>
+#include <iostream>
 #include "line.hpp"
 using namespace std;
 
 class Program {
 private:
 	vector<Line*> lines;
+public:
+	Program(istream& fin) {
+		fin.seekg(0, ios::beg);
+		string line;
+		while (getline(fin, line)) {
+			lines.push_back(parse(line));
+		}
+	}
+	~Program() {
+		for (auto i : lines) delete i;
+	}
+
+	Line* parse(string& line) {
+		int t = 0;
+		getToken(line, t);
+
+	}
+
+	inline bool isLetter(char x) {
+		return ((x >= 'a') && (x <= 'z')) || ((x >= 'A') && (x <= 'Z'));
+	}
+
+	inline bool isSpecial(char x) {
+		return (x == '_' || x == '.' || x == ':');
+	}
+
+	string getToken(string& line, int& t) {
+		while (!isLetter(line[t]) && !isSpecial(line[t]) 
+			&& line[t] != ',' && line[t] != '\n') ++t;
+		if (line[t] == '\n') return "";
+		string ret = "";
+		while (isLetter(line[t]) || isSpecial(line[t])) {
+			ret = ret + line[t];
+			++t;
+		}
+		return ret;
+	}
 };
 
 #endif
